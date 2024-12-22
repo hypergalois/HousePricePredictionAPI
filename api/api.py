3f48b07201493d38ddb37cac6cdf1052387665ad
hypergalois <jose.delgado2@live.u-tad.com>
2024-12-22 20:36:37 +0100

    Load pre-trained XGBoost model in FastAPI application

--- api/api.py
+++ api/api.py
@@ -2,3 +2,9 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import dill
 import pandas as pd
+
+app = FastAPI()
+
+with open("xgboost_gridsearch.pkl", "rb") as f:
+    model = dill.load(f)
+
